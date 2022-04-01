ff9cf9852bac7cec12d1c4cd7eddee62157c3400
khang7398 <70089835+khang7398@users.noreply.github.com>
2022-04-01 17:42:04 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
-import Form from './component/Form';
-import List from './component/List';
+import React from 'react';
 import './App.css';
+import { useForm } from 'react-hook-form';
 
-export interface IState {
-  people: {
-    name: string;
-    age: number;
-    bio: string;
-  }[];
+interface IFormInput {
+  name: string;
+  password: string;
+  email: string;
 }
-
 export default function App() {
-  const [name, setName] = useState('');
-  const [age, setAge] = useState(5);
-  const [people, setPeople] = useState<IState['people']>([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+  const onSubmit = handleSubmit((data) => {
+    console.log(JSON.stringify(data));
+  });
 
   return (
-    <div className="container">
-      <Form people={people} setPeople={setPeople} />
-      <List people={people} />
-    </div>
+    <form onSubmit={onSubmit}>
+      <h1>React Hook Form</h1>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        placeholder="Your Name"
+        {...register('name', { required: true })}
+      />
+      {errors.name && <span className="error-container">Name is required</span>}
+      <label htmlFor="name">Password</label>
+      <input
+        type="password"
+        id="password"
+        placeholder="Password"
+        {...register('password', { required: true, minLength: 6 })}
+      />
+      {errors.password && <span className="error-container">Password is required</span>}
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="Your Email"
+        {...register('email', {
+          required: true,
+          pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        })}
+      />
+      {errors.email && <span className="error-container">Email is required</span>}
+      <button type="submit">Submit</button>
+    </form>
   );
 }
